d46eee95102355dc8764f5d42a0cb19cdb611082
tesu123 <abhijit.rabidas.mca@gmail.com>
2025-01-05 02:48:29 +0530

    Update send-email.js

--- api/send-email.js
+++ api/send-email.js
@@ -27,6 +27,6 @@ export default async function handler(req, res) {
     res.status(200).json({ message: "Email sent successfully." });
   } catch (error) {
     console.error("Error sending email:", error);
-    res.status(500).json({ message: "Failed to send email." });
+    res.status(500).json({ message: "Failed to email." });
   }
 }
